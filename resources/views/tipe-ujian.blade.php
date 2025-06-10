e81916b2b9e826a2a630bff80d2a4adc8a96cf4c
danesdeova01 <151486791+danesdeova01@users.noreply.github.com>
2025-06-10 19:46:39 +0700

    Update tipe-ujian.blade.php

--- resources/views/tipe-ujian.blade.php
+++ resources/views/tipe-ujian.blade.php
@@ -3,26 +3,25 @@
 @section('title', 'Pilih Tipe Ujian')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-6 mb-3">
-            <a href="{{ route('kuis.mulai', ['mapel_id' => $mapel_id, 'tipe' => 'UTS']) }}">
-                <div class="card text-white bg-primary text-center">
-                    <div class="card-body">
-                        <h3>UTS</h3>
-                    </div>
+    <div class="container">
+        <div class="row">
+            @php
+                $jenisUjianList = \App\Models\JenisUjian::all();
+                $colClass = 'col-md-' . 12 / max(1, $jenisUjianList->count());
+            @endphp
+
+            @foreach ($jenisUjianList->sortByDesc('id') as $jenisUjian)
+                <div class="{{ $colClass }} mb-3">
+                    <a href="{{ route('kuis.mulai', ['mapel_id' => $mapel_id, 'tipe' => $jenisUjian->nama]) }}">
+                        <div class="card text-white bg-primary text-center">
+                            <div class="card-body">
+                                <h3>{{ $jenisUjian->nama }}</h3>
+                                <p class="mb-0">{{ $jenisUjian->timer }} menit</p>
+                            </div>
+                        </div>
+                    </a>
                 </div>
-            </a>
-        </div>
-        <div class="col-md-6 mb-3">
-            <a href="{{ route('kuis.mulai', ['mapel_id' => $mapel_id, 'tipe' => 'UAS']) }}">
-                <div class="card text-white bg-primary text-center">
-                    <div class="card-body">
-                        <h3>UAS</h3>
-                    </div>
-                </div>
-            </a>
+            @endforeach
         </div>
     </div>
-</div>
 @endsection
